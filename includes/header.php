d1f6a1257c65da99e5c7ef8aaa8fb1e8bec9b715
geeksiah <kojocreateve@gmail.com>
2025-10-08 01:43:08 +0000

    ui 11

--- includes/header.php
+++ includes/header.php
@@ -17,6 +17,7 @@ require_once __DIR__ . '/../config/config.php';
     })();
   </script>
   <link rel="stylesheet" href="/assets/css/style.css">
+  <link rel="stylesheet" href="/assets/css/dashboard.css">
   <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
   <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.css">
 </head>
